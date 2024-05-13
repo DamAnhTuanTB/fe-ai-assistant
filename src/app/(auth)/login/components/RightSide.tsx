00fa6353c9ed883759abdeb60084095e99670f25
Joaquin-Tuan <joaquin@metalabs.company>
2024-05-13 17:54:55 +0700

    Fix bug:

--- src/app/(auth)/login/components/RightSide.tsx
+++ src/app/(auth)/login/components/RightSide.tsx
@@ -27,11 +27,9 @@ export default function RightSide() {
       .required("Password is required to input"),
   });
   const {
-    control,
     handleSubmit,
     register,
     formState: { errors },
-    reset,
   } = useForm({
     resolver: yupResolver(schema),
     mode: "onChange",
@@ -49,84 +47,87 @@ export default function RightSide() {
           md: "40%",
         },
         p: "32px",
+        pr: "0px",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
       }}
     >
-      <Logo sx={{ display: { base: "flex", md: "none" } }} />
-      <Text
-        sx={{
-          fontSize: "28px",
-          mt: {
-            base: "24px",
-            md: "0px",
-          },
-          mb: "28px",
-          fontWeight: "medium",
-          color: "green.800",
-        }}
-      >
-        <StarIcon
-          color="green.300"
-          sx={{ display: { base: "none", md: "inline-block" } }}
-        />{" "}
-        Login to use more features
-      </Text>
-      <form
-        noValidate
-        onSubmit={handleSubmit(onSubmit)}
-        style={{ width: "100%" }}
-      >
-        <CustomTextInput
-          label="Email"
-          error={errors["email"]}
-          register={register("email")}
-          isRequired={true}
-        />
-        <CustomPasswordInput
-          label="Password"
-          error={errors["password"]}
-          register={register("password")}
-          isRequired={true}
-        />
-        <Button
-          type="submit"
-          size="lg"
-          width="100%"
-          colorScheme="teal"
-          sx={{ mt: "16px" }}
+      <Box sx={{ maxHeight: "100vh", overflowY: "auto", pr: "32px" }}>
+        <Logo sx={{ display: { base: "flex", md: "none" } }} />
+        <Text
+          sx={{
+            fontSize: "28px",
+            mt: {
+              base: "24px",
+              md: "0px",
+            },
+            mb: "28px",
+            fontWeight: "medium",
+            color: "green.800",
+          }}
         >
-          Login
-        </Button>
-      </form>
-      <Box position="relative" pt={10} pb={10}>
-        <Divider />
-        <AbsoluteCenter bg="white" px="4">
-          or
-        </AbsoluteCenter>
-      </Box>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: "16px" }}>
-        <Button
-          size="lg"
-          colorScheme="teal"
-          variant="outline"
-          leftIcon={<Image src="/images/google.png" alt="" width={30} />}
+          <StarIcon
+            color="green.300"
+            sx={{ display: { base: "none", md: "inline-block" } }}
+          />{" "}
+          Login to use more features
+        </Text>
+        <form
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+          style={{ width: "100%" }}
         >
-          <Text width={220} textAlign="left">
-            Continue with Google
-          </Text>
-        </Button>
-        <Button
-          size="lg"
-          colorScheme="teal"
-          variant="outline"
-          leftIcon={<Image src="/images/facebook.svg" alt="" width={30} />}
-        >
-          <Text width={220} textAlign="left">
-            Continue with Facebook
-          </Text>
-        </Button>
+          <CustomTextInput
+            label="Email"
+            error={errors["email"]}
+            register={register("email")}
+            isRequired={true}
+          />
+          <CustomPasswordInput
+            label="Password"
+            error={errors["password"]}
+            register={register("password")}
+            isRequired={true}
+          />
+          <Button
+            type="submit"
+            size="lg"
+            width="100%"
+            colorScheme="teal"
+            sx={{ mt: "16px" }}
+          >
+            Login
+          </Button>
+        </form>
+        <Box position="relative" pt={10} pb={10}>
+          <Divider />
+          <AbsoluteCenter bg="white" px="4">
+            or
+          </AbsoluteCenter>
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+          <Button
+            size="lg"
+            colorScheme="teal"
+            variant="outline"
+            leftIcon={<Image src="/images/google.png" alt="" width={30} />}
+          >
+            <Text width={220} textAlign="left">
+              Continue with Google
+            </Text>
+          </Button>
+          <Button
+            size="lg"
+            colorScheme="teal"
+            variant="outline"
+            leftIcon={<Image src="/images/facebook.svg" alt="" width={30} />}
+          >
+            <Text width={220} textAlign="left">
+              Continue with Facebook
+            </Text>
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
