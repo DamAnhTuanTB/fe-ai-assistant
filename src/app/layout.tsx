17126913295e38e293781789e438921c5580b908
Joaquin-Tuan <joaquin@metalabs.company>
2024-05-30 16:26:42 +0700

    Fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,24 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI Assistant",
+  title: "AI Assistant - Explore the world of AI",
   description: "Explore the world of AI with our AI assistant.",
+  openGraph: {
+    title: "AI Assistant - Explore the world of AI",
+    description: "Explore the world of AI with our AI assistant.",
+    url: process.env.NEXT_PUBLIC_URL,
+    siteName: "AI Assistant",
+    images: [
+      {
+        url: "https://assets-global.website-files.com/645523c36dce1ac1ed9106e2/64dde1726d446b169fb8d05d_GPT-3%20Personal%20Assistant.png", // Must be an absolute URL
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  alternates: {
+    canonical: process.env.NEXT_PUBLIC_URL,
+  },
 };
 
 export default function RootLayout({
